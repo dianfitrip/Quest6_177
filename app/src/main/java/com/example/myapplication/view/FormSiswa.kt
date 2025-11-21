e6515b328374f11462dce8571190700dc65e01c5
dianfitrip <dianfitri683@gmail.com>
2025-11-21 10:55:01 +0700

    edit bagian gender, menambahkan txt gender

--- app/src/main/java/com/example/myapplication/view/FormSiswa.kt
+++ app/src/main/java/com/example/myapplication/view/FormSiswa.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -76,13 +77,21 @@ fun FormIsian(
                 .padding(all = 20.dp)
                 .width(width = 250.dp), thickness = Thickness,color = Color.Red)
             Row{
-                jenisK.forEach{
-                        item->
-                    Row(verticalAlignment = Alignment.CenterVertically){
+                jenisK.forEach { item ->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender == item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                    verticalAlignment = Alignment.CenterVertically){
                         RadioButton(
-                            selected = false,
-                            onClick = {item}
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
                         )
+
                         Text(text = item)
                     }
                 }
